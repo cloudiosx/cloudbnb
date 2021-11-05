3f57bc87588d8c124adbedcd84adb50209e0984d
John Kim <cloudiosx@gmail.com>
2021-11-05 06:52:49 -0400

    Complete LoginFormPage

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,4 +1,3 @@
-// import { useReducer } from "react";
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import sessionReducer from "./session";
