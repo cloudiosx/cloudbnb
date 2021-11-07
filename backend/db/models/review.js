42604d340cf97a38456b17e6864f478387e2c848
John Kim <cloudiosx@gmail.com>
2021-11-07 01:38:18 -0400

    Set-up  review table, and establish relationship between user table and review table

---
+++ backend/db/models/review.js
@@ -0,0 +1,25 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Review = sequelize.define(
+    "Review",
+    {
+      userId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      homesId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      review: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+    },
+    {}
+  );
+  Review.associate = function (models) {
+    Review.belongsTo(models.User, { foreignKey: "userId" });
+  };
+  return Review;
+};
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -51,7 +51,7 @@ module.exports = (sequelize, DataTypes) => {
   );
 
   User.associate = function (models) {
-    // associations can be defined here
+    User.hasMany(models.Review, { foreignKey: "userId" });
   };
 
   User.prototype.toSafeObject = function () {
