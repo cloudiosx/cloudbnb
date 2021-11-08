3a3f2c6eb77fae3712d00821c1b1f2167d4c6c9c
John Kim <cloudiosx@gmail.com>
2021-11-08 17:20:54 -0500

    Add description column into mHomes table

---
+++ backend/db/seeders/20211108184924-home.js
@@ -0,0 +1,69 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert(
+      "Homes",
+      [
+        {
+          userId: 1,
+          description: "Hi A",
+          name: "HOUSE A",
+          address: "Address A",
+          city: "City A",
+          state: "State A",
+          country: "Country A",
+          price: "1000",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 2,
+          description: "Hi B",
+          name: "HOUSE B",
+          address: "Address B",
+          city: "City B",
+          state: "State B",
+          country: "Country B",
+          price: "2000",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 3,
+          description: "Hi C",
+          name: "HOUSE C",
+          address: "Address C",
+          city: "City C",
+          state: "State C",
+          country: "Country C",
+          price: "3000",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete("Homes", null, {});
+  },
+};
