16f9948ab4822f34799134b627f5bbc293f6c79e
John Kim <cloudiosx@gmail.com>
2021-11-08 06:25:25 -0500

    Complete banner button and add Google Font to public index.html file

--- frontend/src/components/Banner/index.js
+++ frontend/src/components/Banner/index.js
@@ -4,7 +4,15 @@ import "./Banner.css";
 function Banner() {
   return (
     <div className="banner">
-      <div></div>
+      <div className="banner__info">
+        <h1>Get out and stretch your imagination</h1>
+        <h5>
+          Plan a different kind of getaway to uncover the hidden gems near you
+        </h5>
+        <button type="button" className="banner__button">
+          <span>I'm flexible</span>
+        </button>
+      </div>
     </div>
   );
 }
