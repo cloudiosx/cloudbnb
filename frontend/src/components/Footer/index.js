6b740142b7f61a99f1f66c4bd30a859e9401f751
John Kim <cloudiosx@gmail.com>
2021-11-14 02:31:04 -0500

    Modify footer and navigation bar

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -5,7 +5,6 @@ function Footer() {
   return (
     <div className="footer">
       <p>© 2021 Cloudbnb USA, Inc. All rights reserved.</p>
-      <p>Privacy Policy · Terms of Use · Accessibility · Sitemap</p>
     </div>
   );
 }
